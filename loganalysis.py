95f871ba4c065c4bd3e9e50bc8d553be5b1dbd62
Mohamad shybly <mhd.shibly@hotmail.com>
2019-07-26 20:39:38 +0200

    Fixed the output to be a readable text , and added try except block to connect to the database

--- loganalysis.py
+++ loganalysis.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
 
 import psycopg2
+import sys
 
-db = psycopg2.connect("dbname=news")
+try:
+    db = psycopg2.connect("dbname=news")
+except psycopg2.Error as e:
+    print("Unable to connect to the database")
+    print(e.pgerror)
+    print(e.diag.message_detail)
+    sys.exit(1)
+
 cursor = db.cursor()
 
 helper1_view = """create view helper1 as
@@ -25,7 +33,8 @@ cursor.execute(helper1_view)
 cursor.execute(first_question_query)
 print("\n----What are the most popular three articles of all time?-----\n")
 result1 = list(cursor.fetchall())
-print(result1)
+for i in range(len(result1)):
+    print(str(result1[i][0]) + " with " + str(result1[i][1]) + " views.")
 
 # Second Question : Who are the most popular article authors of all time?
 
@@ -37,7 +46,8 @@ second_question_query = """select author_name,count(*)as num from helper1
 cursor.execute(second_question_query)
 result2 = list(cursor.fetchall())
 print("\n----Who are the most popular article authors of all time?-----\n")
-print(result2)
+for i in range(len(result2)):
+    print(str(result2[i][0]) + " with " + str(result2[i][1]) + " views.")
 
 # Third Question : On which days did more than 1% of requests lead to errors?
 
@@ -48,15 +58,13 @@ cursor.execute("""select day,status,count(*) as sum
 
 tmp_array = cursor.fetchall()
 result3 = []
+print("\n--On which days did more than 1% of requests lead to errors?--\n")
 for i in range(0, len(tmp_array), 2):
     number_of_200 = tmp_array[i][2]
     number_of_404 = tmp_array[i + 1][2]
     tmp_sum = number_of_200 + number_of_404
     percentage = float(number_of_404 * 100) / float(tmp_sum)
     if (percentage) > 1:
-        result3.append((str(tmp_array[i][0]), str(percentage) + ' %'))
-
-print("\n--On which days did more than 1% of requests lead to errors?--\n")
-print(result3)
-
+        print("("+str(tmp_array[i][0])+")" + " with " + "("
+              + str(percentage)+")"+"%")
 db.close()
